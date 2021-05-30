6b2c73e5e69698e15b25b5e151cc27bae84e9ee1
praveen tailor <praveentailor4920@gmail.com>
2021-05-30 06:37:59 +0530

    debug

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: true,
+    rejectUnauthorized: false
   }
 });
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 const app = express();
 
