616174c8def34b6a33559bb7bf1b7ea1a539ea44
praveen tailor <praveentailor4920@gmail.com>
2021-05-30 05:30:36 +0530

    change host

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-rigid-40672',
     user: 'postgres',
     password: '8952',
     database: 'smartbrain'
